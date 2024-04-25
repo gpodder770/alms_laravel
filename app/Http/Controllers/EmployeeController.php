45dd212dcb5bf7437191e134d486712d6d7cffef
gpodder770 <71787759+gpodder770@users.noreply.github.com>
2024-04-25 16:56:21 +0600

    fixed employee leave apply page

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -12,7 +12,6 @@ use DB;
 use App\Models\Employee;
 use App\Models\EmployeesLeave;
 use App\Models\Holidays;
-use App\Models\Holidays_view;
 use App\Models\LeavePolicy;
 
 class EmployeeController extends Controller
@@ -30,6 +29,7 @@ class EmployeeController extends Controller
 
     //todo: admin login functionality
     public function login_submit(Request $request){
+        // dd($request);
         $request->validate([
             'email'=>'required',
             'password'=>'required',
@@ -51,11 +51,7 @@ class EmployeeController extends Controller
 
     public function dashboard()
     {
-        $current_year = Carbon::now()->format('Y');
-        $today_date = Carbon::now()->format('Y-m-d');
-        $holidays = Holidays_view::where('company_location',Auth::guard('employee')->user()->company_location)->where('date','>=',$today_date)->take(5)->get();
-        $all_leave_info = EmployeesLeave::where('e_id',Auth::guard('employee')->user()->id)->whereYear('start_date',$current_year)->latest()->take(5)->get();
-        return view('employee.dashboard',compact('holidays','all_leave_info'));
+        return view('employee.dashboard');
     }
 
     public function profile(){
@@ -101,7 +97,8 @@ class EmployeeController extends Controller
         return view('employee.attendance');
     }
     public function apply_leave(){
-        $current_year = Carbon::now()->format('Y');
+        // $old_leave_info = EmployeesLeave::where('e_id',Auth::guard('employee')->user()->id)->get();
+        $all_leave_info = EmployeesLeave::where('e_id',Auth::guard('employee')->user()->id)->whereYear('created_at', Carbon::now()->format('Y'))->get();
         if(Auth::guard('employee')->user()->company_location == 0){
             $leave_policy = LeavePolicy::where('company_location',0)->first();
             $sick = $leave_policy->sick;
@@ -109,14 +106,34 @@ class EmployeeController extends Controller
             $casual = $leave_policy->casual;
             $special = $leave_policy->special;
             $earned = $leave_policy->earned;
-            $already_taken_leave = EmployeesLeave::select('leave_type',DB::raw("SUM(number_of_days) as total_number_of_days"))->where(['e_id'=>Auth::guard('employee')->user()->id,'status'=>1])->whereYear('start_date',$current_year)->groupBy('leave_type')->get();
-            
-            foreach ($already_taken_leave as $each_already_taken_leave) {
-                $sick = $each_already_taken_leave->leave_type == 0 ? ($sick - $each_already_taken_leave->total_number_of_days) : $sick;
-                $public = $each_already_taken_leave->leave_type == 1 ? ($public - $each_already_taken_leave->total_number_of_days) : $public;
-                $casual = $each_already_taken_leave->leave_type == 2 ? ($casual - $each_already_taken_leave->total_number_of_days) : $casual;
-                $special = $each_already_taken_leave->leave_type == 3 ? ($special - $each_already_taken_leave->total_number_of_days) : $special;
-                $earned = $each_already_taken_leave->leave_type == 4 ? ($earned - $each_already_taken_leave->total_number_of_days) : $earned;
+
+            foreach($all_leave_info as $each_leave_info){
+                if($each_leave_info->leave_type == 0){
+                    $sick = $sick - $each_leave_info->number_of_days;
+                    if($sick < 0 ){
+                        $sick = 0;
+                    }
+                }elseif($each_leave_info->leave_type == 1){
+                    $public = $public - $each_leave_info->number_of_days;
+                    if($public < 0 ){
+                        $public = 0;
+                    }
+                }elseif($each_leave_info->leave_type == 2){
+                    $casual = $casual - $each_leave_info->number_of_days;
+                    if($casual < 0 ){
+                        $casual = 0;
+                    }
+                }elseif($each_leave_info->leave_type == 3){
+                    $special = $special - $each_leave_info->number_of_days;
+                    if($special < 0 ){
+                        $special = 0;
+                    }
+                }else{
+                    $earned = $earned - $each_leave_info->number_of_days;
+                    if($earned < 0 ){
+                        $earned = 0;
+                    }
+                }
             }
         }else{
             $leave_policy = LeavePolicy::where('company_location',1)->first();
@@ -125,53 +142,55 @@ class EmployeeController extends Controller
             $casual = $leave_policy->casual;
             $special = $leave_policy->special;
             $earned = $leave_policy->earned;
-            $already_taken_leave = EmployeesLeave::select('leave_type',DB::raw("SUM(number_of_days) as total_number_of_days"))->where(['e_id'=>Auth::guard('employee')->user()->id,'status'=>1])->whereYear('start_date',$current_year)->groupBy('leave_type')->get();
-            
-            foreach ($already_taken_leave as $each_already_taken_leave) {
-                $sick = $each_already_taken_leave->leave_type == 0 ? ($sick - $each_already_taken_leave->total_number_of_days) : $sick;
-                $public = $each_already_taken_leave->leave_type == 1 ? ($public - $each_already_taken_leave->total_number_of_days) : $public;
-                $casual = $each_already_taken_leave->leave_type == 2 ? ($casual - $each_already_taken_leave->total_number_of_days) : $casual;
-                $special = $each_already_taken_leave->leave_type == 3 ? ($special - $each_already_taken_leave->total_number_of_days) : $special;
-                $earned = $each_already_taken_leave->leave_type == 4 ? ($earned - $each_already_taken_leave->total_number_of_days) : $earned;
+
+            foreach($all_leave_info as $each_leave_info){
+                if($each_leave_info->leave_type == 0){
+                    $sick = $sick - $each_leave_info->number_of_days;
+                    if($sick < 0 ){
+                        $sick = 0;
+                    }
+                }elseif($each_leave_info->leave_type == 1){
+                    $public = $public - $each_leave_info->number_of_days;
+                    if($public < 0 ){
+                        $public = 0;
+                    }
+                }elseif($each_leave_info->leave_type == 2){
+                    $casual = $casual - $each_leave_info->number_of_days;
+                    if($casual < 0 ){
+                        $casual = 0;
+                    }
+                }elseif($each_leave_info->leave_type == 3){
+                    $special = $special - $each_leave_info->number_of_days;
+                    if($special < 0 ){
+                        $special = 0;
+                    }
+                }else{
+                    $earned = $earned - $each_leave_info->number_of_days;
+                    if($earned < 0 ){
+                        $earned = 0;
+                    }
+                }
             }
         }
-        $all_leave_info = EmployeesLeave::where('e_id',Auth::guard('employee')->user()->id)->whereYear('start_date',$current_year)->latest()->get();
-        return view('employee.apply_leave',compact('all_leave_info','sick','public','casual','special','earned'));
+        return view('employee.apply_leave',compact('leave_policy','all_leave_info','sick','public','casual','special','earned'));
     }
 
     public function apply_leave_submit(Request $request){
         $request->validate([
             'leave_type'=>'required|numeric',
-            'start_date'=>'required|date|after:yesterday',
+            'start_date'=>'required|date',
             'leave_reason'=>'required',
         ]);
 
         if($request->enddate){
             $request->validate([
-                'end_date'=>'date|after:yesterday'
+                'end_date'=>'date'
             ]);
         }
         
         if(Auth::guard('employee')->user()->company_location == 0){
-            if($request->leave_type == 5){
-                $diffInDays = 0.5;
-                $data = [
-                    'e_id'=>Auth::guard('employee')->user()->id,
-                    'leave_type'=>2,
-                    'start_date'=>$request->start_date,
-                    'end_date'=>$request->start_date,
-                    'number_of_days'=>$diffInDays,
-                    'leave_reason'=>$request->leave_reason,
-                    'status'=>0,
-                ];
-    
-                $create_leave = EmployeesLeave::create($data);
-                if ($create_leave) {
-                    return back()->with('success', 'Leave Applied Successfully');
-                } else {
-                    return back()->with('error', 'Something went Wrong!');
-                }
-            }elseif(($request->start_date == $request->end_date) || ($request->start_date && is_null($request->end_date))){
+            // Bangladesh Employees
+            if(($request->start_date == $request->end_date) || ($request->start_date && is_null($request->end_date))){
                 $diffInDays = 1;
                 $data = [
                     'e_id'=>Auth::guard('employee')->user()->id,
@@ -196,23 +215,10 @@ class EmployeeController extends Controller
                 ]);
                 $startDate = Carbon::parse($request->start_date);
                 $endDate = Carbon::parse($request->end_date);
-                $diffInDays = $endDate->diffInDays($startDate) + 1;
+                $diffInDays = $startDate->diffInDays($endDate) + 1;
                 $num_of_weekend_between_dates = $startDate->diffInDaysFiltered(function (Carbon $date){return !$date->isWeekday();}, $endDate);
-                $govt_holidays = Holidays_view::where('company_location',0)->whereBetween('date',[$request->start_date,$request->end_date])->get();
-                $num_of_govt_holidays = $govt_holidays->count();
-                $num_of_govt_holidays_without_weekend = 0;
-                if($num_of_govt_holidays == 0){
-                    $total_num_of_holidays = $diffInDays - $num_of_weekend_between_dates - $num_of_govt_holidays;
-                }else{
-                    foreach ($govt_holidays as $each_num_of_govt_holidays) {
-                        $day_date = Carbon::createFromFormat('Y-m-d', $each_num_of_govt_holidays->date)->format('l');
-
-                        if($day_date == "Friday" || $day_date == "Saturday"){
-                            $num_of_govt_holidays_without_weekend = $num_of_govt_holidays - 1;
-                        }
-                    }
-                    $total_num_of_holidays = $diffInDays - $num_of_weekend_between_dates - $num_of_govt_holidays_without_weekend;
-                }
+                $num_of_govt_holidays = Holidays::where('company_location',0)->whereBetween('date',[$request->start_date,$request->end_date])->get()->count();
+                $total_num_of_holidays = $diffInDays - $num_of_weekend_between_dates - $num_of_govt_holidays;
                 $data = [
                     'e_id'=>Auth::guard('employee')->user()->id,
                     'leave_type'=>$request->leave_type,
@@ -222,6 +228,7 @@ class EmployeeController extends Controller
                     'leave_reason'=>$request->leave_reason,
                     'status'=>0,
                 ];
+
                 $create_holiday = EmployeesLeave::create($data);
 
                 if ($create_holiday) {
@@ -232,25 +239,7 @@ class EmployeeController extends Controller
             }
         }else{
             // India Employees
-            if($request->leave_type == 5){
-                $diffInDays = 0.5;
-                $data = [
-                    'e_id'=>Auth::guard('employee')->user()->id,
-                    'leave_type'=>2,
-                    'start_date'=>$request->start_date,
-                    'end_date'=>$request->start_date,
-                    'number_of_days'=>$diffInDays,
-                    'leave_reason'=>$request->leave_reason,
-                    'status'=>0,
-                ];
-    
-                $create_leave = EmployeesLeave::create($data);
-                if ($create_leave) {
-                    return back()->with('success', 'Leave Applied Successfully');
-                } else {
-                    return back()->with('error', 'Something went Wrong!');
-                }
-            }elseif(($request->start_date == $request->end_date) || ($request->start_date && is_null($request->end_date))){
+            if(($request->start_date == $request->end_date) || ($request->start_date && is_null($request->end_date))){
                 $diffInDays = 1;
                 $data = [
                     'e_id'=>Auth::guard('employee')->user()->id,
@@ -277,21 +266,8 @@ class EmployeeController extends Controller
                 $endDate = Carbon::parse($request->end_date);
                 $diffInDays = $startDate->diffInDays($endDate) + 1;
                 $num_of_weekend_between_dates = $startDate->diffInDaysFiltered(function (Carbon $date){return !$date->isWeekday();}, $endDate);
-                $govt_holidays = Holidays_view::where('company_location',1)->whereBetween('date',[$request->start_date,$request->end_date])->get();
-                $num_of_govt_holidays = $govt_holidays->count();
-                $num_of_govt_holidays_without_weekend = 0;
-                if($num_of_govt_holidays == 0){
-                    $total_num_of_holidays = $diffInDays - $num_of_weekend_between_dates - $num_of_govt_holidays;
-                }else{
-                    foreach ($govt_holidays as $each_num_of_govt_holidays) {
-                        $day_date = Carbon::createFromFormat('Y-m-d', $each_num_of_govt_holidays->date)->format('l');
-
-                        if($day_date == "Saturday" || $day_date == "Sunday"){
-                            $num_of_govt_holidays_without_weekend = $num_of_govt_holidays - 1;
-                        }
-                    }
-                    $total_num_of_holidays = $diffInDays - $num_of_weekend_between_dates - $num_of_govt_holidays_without_weekend;
-                }
+                $num_of_govt_holidays = Holidays::where('company_location',1)->whereBetween('date',[$request->start_date,$request->end_date])->get()->count();
+                $total_num_of_holidays = $diffInDays - $num_of_weekend_between_dates - $num_of_govt_holidays;
                 $data = [
                     'e_id'=>Auth::guard('employee')->user()->id,
                     'leave_type'=>$request->leave_type,
